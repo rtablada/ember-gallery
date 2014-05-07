e40e7ba00ca90902536955a932ad39828728c99a
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-05-06 22:08:26 -0500

    Datbase config update

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => 'pgsql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -66,9 +66,9 @@ return array(
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
+			'database' => 'ember_gallery',
+			'username' => 'ember_gallery',
+			'password' => 'ember_gallery',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
